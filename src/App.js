6597107e3485f3eb346253a486404e4b699c1b25
Kamyar Nadjafloo <knadjafloo@gmail.com>
2019-01-03 10:24:49 -0800

    added calculate winner

--- src/App.js
+++ src/App.js
@@ -24,15 +24,6 @@ class App extends Component {
           msg: [...prevState.msg, "Setting value to " + playerSymbol, playMsg],
           player: !playerTurn
         }));
-
-        if (this.calculateWinner(this.state.values)) {
-          this.setState(prevState => ({
-            msg: [
-              ...prevState.msg,
-              "Player " + playerTurn + 1 + " has WON the game!!!!"
-            ]
-          }));
-        }
         break;
       case "X":
         curValue = null;
@@ -46,11 +37,38 @@ class App extends Component {
 
     values[index] = curValue;
     this.setState({ values: values });
+    if (this.calculateWinner(values)) {
+      this.setState(prevState => ({
+        msg: [
+          ...prevState.msg,
+          "Player " + (playerTurn ? "1" : "2") + " has WON the game!!!!"
+        ]
+      }));
+    }
   }
 
-  calculateWinner(values) {
-    //TODO
-    return false;
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
   renderCell(index) {
